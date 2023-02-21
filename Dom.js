65e2b4a9065954a590730eeb7ba7eca04195f416
rakeshdkd <rakeshd1012@gmail.com>
2023-02-21 18:34:00 +0530

    first commit.

---
+++ Dom.js
@@ -0,0 +1,49 @@
+// EXAMINE THE DOCUMENT OBJECTS.
+// console.log(document.domain);
+// console.log(document.URL);
+// console.log(document.title);
+// document.title="dom manipulation"
+
+// console.log(document.doctype);
+// console.log(document.head);
+// console.log(document.body);
+// console.log(document.all);
+// console.log(document.all[10]);
+// console.log(document.textContent="Hello....!");
+// console.log(document.forms[0]);
+// console.log(document.links);
+// console.log(document.images);
+
+
+
+// GET ELEMENT BY ID.
+// const heading = document.getElementById('main-header');
+// heading.innerText = 'hELLO....!';
+// heading.textContent = 'gOODBYE...!';
+// heading.innerHTML = "<h2>Yeaiiiii...!</h2>"
+// heading.innerHTML = "Yeaiiiii...!";
+
+// heading.style.borderBottom = "8px solid black"
+// console.log(heading);
+
+// GET ELEMENT BY CLASS NAME.
+
+var title= document.getElementsByClassName('title');
+console.log(title);
+console.log(title[0]);
+
+title[0].style.color='green'
+title[0].style.fontWeight='bold'
+
+
+
+
+
+
+
+
+
+
+
+
+
--- demo.js
+++ demo.js
@@ -1,3 +1,4 @@
 
 
  console.log('i love coding !')
+ console.log(" sometime i hate to do according to any video, but it cant happen the same") 
