c67f503b89683fe8a87afacbb58d594c231b8f99
rakeshdkd <rakeshd1012@gmail.com>
2023-02-22 17:28:10 +0530

    Creating nodes and modifying Dom

--- Dom.js
+++ Dom.js
@@ -92,16 +92,131 @@
 
 // GET ELEMENT BY QUERYSELECTORALL:-
 
-var item = document.querySelectorAll('.list-group-item');
-item[1].style.color='green';
+// var item = document.querySelectorAll('.list-group-item');
+// item[1].style.color='green';
 
-var items = document.querySelectorAll('.list-group-item:nth-child(odd)');
-for(let i=0; i<items.length; i++){
-items[i].style.backgroundColor='green'
-}
-
-
+// var items = document.querySelectorAll('.list-group-item:nth-child(odd)');
+// for(let i=0; i<items.length; i++){
+// items[i].style.backgroundColor='green'
+// }
 
 
+// TRAVERS THE DOM:
+
+var itemList = document.querySelector('#items');
+
+// Parent node :- This is a very important thing to get any parent node.
+// console.log(itemList.parentNode);
+// itemList.parentNode.style.backgroundColor="#cbc"
+// console.log(itemList.parentNode.parentNode);
+// console.log(itemList.parentNode.parentNode.parentNode);
+// console.log(itemList.parentNode.parentNode.parentNode.parentNode);
+// console.log(itemList.parentNode.parentNode.parentNode.parentNode.parentNode);
+// console.log(itemList.parentNode.parentNode.parentNode.parentNode.parentNode.parentNode);
+
+// Prent element
+console.log(itemList.parentElement);
+// itemList.parentNode.style.backgroundColor="#cbc"
+// console.log(itemList.parentElement.parentElement);
+// console.log(itemList.parentElement.parentElement.parentElement);
+// console.log(itemList.parentElement.parentElement.parentElement.parentElement);
+// console.log(itemList.parentElement.parentElement.parentElement.parentElement.parentElement);
+
+// child node: This may create some problems so avoide using this, instead use children
+// console.log(itemList.childNodes); // it show line break as childnodes this may creat problem to you.
 
 
+// // CHILDREN :-
+
+// console.log(itemList.children); // it gives us the childrens only. HTML collection.
+// console.log(itemList.children[1]);
+// itemList.children[0].style.backgroundColor="yellow"
+
+// FIRSTchild:-
+// console.log(itemList.firstChild)
+// itemList.firstElementChild.textContent="Hello...!"
+
+// firstElementChild:-
+// console.log(itemList.firstElementChild);
+
+// // lastChild:-
+// console.log(itemList.lastChild) // this gives the type of child element
+
+// // lastElementChild:-
+// console.log(itemList.lastElementChild); // this gives Whole element.
+// itemList.lastElementChild.textContent="hello...!"
+
+// nextSibling:
+// console.log(itemList.nextSibling); // try not to use this
+
+// // nextElementSibling:
+// console.log(itemList.nextElementSibling); // always try to use this only.
+
+// // previoussibling:-
+// console.log(itemList.previousSibling);
+
+// // previousElementsibling:-
+// console.log(itemList.previousElementSibling);
+// itemList.previousElementSibling.style.color="red"
+
+/*
+// CREATE ELEMENT : yaha hum element create kr rhe h.
+var newDiv = document.createElement('div');
+// add class to it 
+newDiv.className = "hello1";
+
+// add id to it
+newDiv.id = "hello";
+
+// add atribute to it:- The setAttribute method is a JavaScript method that is used to set the value of a specified attribute on a given element in the Document Object Model (DOM) yaha hum class ko ese bhi add kr skte the newDiv.setAttribute('class', 'hello1');.
+newDiv.setAttribute('title', 'Hello div1');
+console.log(newDiv);
+
+
+// create text node: yaha hum text node bana rhe h 
+var newDivText = document.createTextNode('Hello world');
+console.log(newDivText)
+
+// Add text to newDiv. yaha hum text ko element m append kr rhe h.
+newDiv.appendChild(newDivText);
+console.log(newDiv)
+
+var container = document.querySelector('header .container');
+var h1 = document.querySelector('header h1');
+
+// How to add any element
+container.insertBefore(newDiv, h1)
+newDiv.style.fontWeight="bold"
+newDiv.style.color="red"
+
+//-----------------
+
+var newDiv1 = document.createElement('div');
+// add class to it 
+newDiv1.className = "hello";
+
+// add id to it
+newDiv1.id = "hello1";
+
+// add atribute to it
+newDiv1.setAttribute('title', 'Hello div');
+
+
+// create text node: yaha hum text node bana rhe h 
+var newDivText1 = document.createTextNode('Hello world...!');
+console.log(newDivText1)
+
+// Add text to newDiv. yaha hum text ko element m append kr rhe h.
+newDiv1.appendChild(newDivText1);
+console.log(newDiv1)
+
+
+var ul = document.querySelector('div .list-group');
+var li = document.querySelector('div li');
+// var itm = document.querySelector('.list-group-item');
+ul.insertBefore(newDiv1,li);
+
+newDiv1.style.fontSize="21px"
+newDiv1.style.fontWeight="bold"
+newDiv1.style.color="blue"
+*/
